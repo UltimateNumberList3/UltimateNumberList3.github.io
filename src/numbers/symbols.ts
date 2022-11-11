3229988cb99288b3fe83a19c3ed6a86efaa8b4c7
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-11-11 16:24:10 +0000

    Symbols added to theme sass

--- src/numbers/symbols.ts
+++ src/numbers/symbols.ts
@@ -4,11 +4,11 @@ function returnSymbols(symbolNumber: ListNumber): string {
     let symbols: string = ""
     for (const symbolI in symbolNumber.symbols) {
         switch (symbolNumber.symbols[symbolI]) {
-            case "milestone": symbols += `<span class="milestone">★</span>`; break
-            case "milestone2": symbols += `<span class="milestone2">✪</span>`; break
-            case "featured": symbols += `<span class="featured">☆</span>`; break
-            case "notknown": symbols += `<span class="notknown">✥</span>`; break
-            default: symbols += `?`; break
+            case "milestone": symbols += `<span class="milestoneSymbol">★</span>`; break
+            case "milestone2": symbols += `<span class="milestone2Symbol">✪</span>`; break
+            case "featured": symbols += `<span class="featuredSymbol">☆</span>`; break
+            case "notknown": symbols += `<span class="notKnownSymbol">✥</span>`; break
+            default: symbols += `<span class="errorSymbol">X</span>`; break
         }
     }
     return symbols
